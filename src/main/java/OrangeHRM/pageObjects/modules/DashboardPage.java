b612297f29fe5845cf693874339b532041c431f7
kcrreddyk <kallemchandrashekarreddy5@gmail.com>
2023-07-17 15:28:21 +0530

    feat(AUTO-19): OHRM: Develop page objects for adminfeat/AUTO-20/automation-script-for-adminmodule-and-test module

--- src/main/java/OrangeHRM/pageObjects/modules/DashboardPage.java
+++ src/main/java/OrangeHRM/pageObjects/modules/DashboardPage.java
@@ -2,7 +2,10 @@ package OrangeHRM.pageObjects.modules;
 
 import OrangeHRM.pageObjects.global.GlobalPageObjects;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class DashboardPage extends GlobalPageObjects {
     WebDriver driver;
@@ -13,9 +16,69 @@ public class DashboardPage extends GlobalPageObjects {
         PageFactory.initElements(driver, this);
     }
 
-    public void dashboardicons(){
+    //TODO - Dashboard Quick Launch
+    @FindBy(xpath = "//button[@class='oxd-icon-button orangehrm-quick-launch-icon' and @title='Assign Leave']")
+    private WebElement assignleave;
 
+    @FindBy(xpath = "//button[@class='oxd-icon-button orangehrm-quick-launch-icon' and @title='Leave List']")
+    private WebElement leavelist;
+
+    @FindBy(xpath = "//button[@class='oxd-icon-button orangehrm-quick-launch-icon' and @title='Timesheets']")
+    private WebElement timesheets;
+
+    @FindBy(xpath = "//button[@class='oxd-icon-button orangehrm-quick-launch-icon' and @title='Apply Leave']")
+    private WebElement applyleave;
+
+    @FindBy(xpath = "//button[@class='oxd-icon-button orangehrm-quick-launch-icon' and @title='My Leave']")
+    private WebElement myleave;
+
+    @FindBy(xpath = "//button[@class='oxd-icon-button orangehrm-quick-launch-icon' and @title='My Timesheet']")
+    private WebElement mytimesheet;
+
+    //TODO - Dashboard My Actions
+    @FindBy(xpath = "(//div[@class='orangehrm-todo-list-item'])[1]")
+    private WebElement leaverequests;
+
+    @FindBy(xpath = "(//div[@class='orangehrm-todo-list-item'])[2]")
+    private WebElement timesheetapprove;
+
+    @FindBy(xpath = "(//div[@class='orangehrm-todo-list-item'])[3]")
+    private WebElement pendingselfreview;
+
+    @FindBy(xpath = "(//div[@class='orangehrm-todo-list-item'])[4]")
+    private WebElement candtointerview;
+
+    public void setQuickLaunch(String QuickLaunch) {
+        if (QuickLaunch.contains("Assign Leave")) {
+            assignleave.click();
+        } else if (QuickLaunch.contains("Leave List")) {
+            leavelist.click();
+        } else if (QuickLaunch.contains("Timesheets")) {
+            timesheets.click();
+        } else if (QuickLaunch.contains("Apply Leave")) {
+            applyleave.click();
+        } else if (QuickLaunch.contains("My Leave")) {
+            myleave.click();
+        } else if (QuickLaunch.contains("My Timesheet")) {
+            mytimesheet.click();
+        } else {
+            System.out.println("Entered Value is not match in Quick Launch");
+            Assert.fail("Entered Value is not match in Quick Launch");
+        }
     }
 
-
+    public void setMyActions(String MyActions) {
+        if (MyActions.contains("Leave Requests")) {
+            leaverequests.click();
+        } else if (MyActions.contains("Timesheets to Approve")) {
+            timesheetapprove.click();
+        } else if (MyActions.contains("Pending Self Review")) {
+            pendingselfreview.click();
+        } else if (MyActions.contains("Candidate to Interview")) {
+            candtointerview.click();
+        } else {
+            System.out.println("Entered Value is not match in My Actions");
+            Assert.fail("Entered Value is not match in My Actions");
+        }
+    }
 }
--- src/main/java/OrangeHRM/pageObjects/modules/admin/AdminOrganization.java
+++ src/main/java/OrangeHRM/pageObjects/modules/admin/AdminOrganization.java
@@ -1,17 +1,22 @@
 package OrangeHRM.pageObjects.modules.admin;
 
 import OrangeHRM.pageObjects.global.GlobalPageObjects;
-import OrangeHRM.utilities.Browser;
-import net.bytebuddy.implementation.bind.annotation.Super;
+import OrangeHRM.utilities.Waits;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import java.util.List;
 
+import static org.openqa.selenium.Keys.CONTROL;
+
 public class AdminOrganization extends GlobalPageObjects {
     WebDriver driver;
+    WebElement OrgResults;
 
     public AdminOrganization(WebDriver driver) {
         super(driver);
@@ -74,7 +79,7 @@ public class AdminOrganization extends GlobalPageObjects {
     @FindBy(css = ".oxd-textarea")
     private WebElement generalnotes;
 
-    @FindBy(css =".oxd-button--medium")
+    @FindBy(css = ".oxd-button--medium")
     private WebElement save;
 
     //TODO - Admin Locations Search PageObjects
@@ -87,13 +92,16 @@ public class AdminOrganization extends GlobalPageObjects {
     @FindBy(xpath = "//div[@class='oxd-select-text oxd-select-text--active']")
     private WebElement locationscountry;
 
+    @FindBy(xpath = "//div[@class='oxd-select-text-input']")
+    private WebElement country;
+
     @FindBy(xpath = "//div[@role='option']")
     private List<WebElement> selectscountry;
 
-    @FindBy(css =".orangehrm-left-space")
+    @FindBy(css = ".orangehrm-left-space")
     private WebElement search;
 
-    @FindBy(css =".oxd-button--ghost")
+    @FindBy(css = ".oxd-button--ghost")
     private WebElement reset;
 
     @FindBy(xpath = "//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")
@@ -128,6 +136,16 @@ public class AdminOrganization extends GlobalPageObjects {
     private WebElement locationsave;
 
     //TODO - Admin Organization Structure Page Objects
+
+    @FindBy(xpath = "//div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--gutters oxd-sheet--pastel-white org-structure-card --edit']")
+    private List<WebElement> structureElements;
+
+    By strDelete = By.xpath("(//div[@class='org-action']/button[@class='oxd-icon-button org-action-icon'])[1]");
+
+    By strEdit = By.xpath("(//div[@class='org-action']/button[@class='oxd-icon-button org-action-icon'])[2]");
+
+    By strAddUnit = By.xpath("(//div[@class='org-action']/button[@class='oxd-icon-button org-action-icon'])[3]");
+
     @FindBy(xpath = "//span[@class='oxd-switch-input oxd-switch-input--active --label-left']")
     private WebElement organizationedit;
 
@@ -146,8 +164,130 @@ public class AdminOrganization extends GlobalPageObjects {
     @FindBy(xpath = ".orangehrm-left-space")
     private WebElement unitsave;
 
-    @FindBy(css =".oxd-dialog-close-button-position")
+    @FindBy(css = ".oxd-dialog-close-button-position")
     private WebElement xclose;
+
+    public void selectCountry(String SelectedCountry) {
+        country.click();
+        WebElement countryresult = selectscountry.stream().filter(country -> country.getText().contains(SelectedCountry)).findFirst().orElse(null);
+        System.out.println(countryresult);
+        countryresult.click();
+    }
+
+    public void accessAndEditGeneralInformation(String GeneralORGName, String GeneralRegistrationNumber, String GeneralTaxID, String GeneralPhone, String GeneralFax, String GeneralAddress1, String GeneralCity, String GeneralProvince, String GeneralPostal, String SelectedCountry, String GeneralNotes) throws InterruptedException {
+        organization.click();
+        Waits.shortPause();
+        generalinformation.click();
+        Waits.shortPause();
+        generaleditcheckbox.click();
+        Waits.shortPause();
+        generalorganizationname.sendKeys(CONTROL, "a", Keys.DELETE);
+        Waits.shortPause();
+        generalorganizationname.sendKeys(GeneralORGName);
+        Waits.shortPause();
+        generalregistrationnumber.sendKeys(GeneralRegistrationNumber);
+        Waits.shortPause();
+        generaltaxid.sendKeys(GeneralTaxID);
+        Waits.shortPause();
+        generalphonenumber.sendKeys(GeneralPhone);
+        Waits.shortPause();
+        generalfax.sendKeys(GeneralFax);
+        Waits.shortPause();
+        generaladdressstreet1.sendKeys(GeneralAddress1);
+        Waits.shortPause();
+        generalcity.sendKeys(GeneralCity);
+        Waits.shortPause();
+        generalprovince.sendKeys(CONTROL, "a", Keys.DELETE);
+        Waits.shortPause();
+        generalprovince.sendKeys(GeneralProvince);
+        Waits.shortPause();
+        generalpostalcode.sendKeys(GeneralPostal);
+        Waits.shortPause();
+        selectCountry(SelectedCountry);
+        Waits.shortPause();
+        generalnotes.sendKeys(GeneralNotes);
+        Waits.shortPause();
+        save.click();
+    }
+
+    public void searchLocations(String LocationName, String SelectedCountry) throws InterruptedException {
+        organization.click();
+        Waits.shortPause();
+        locations.click();
+        Waits.shortPause();
+        locationname.sendKeys(LocationName);
+        Waits.shortPause();
+        selectCountry(SelectedCountry);
+        Waits.shortPause();
+        search.click();
+    }
+
+    public void addLocation(String Name, String City, String Province, String SelectedCountry, String Phone, String Fax, String sendAddress, String LocationNotes) throws InterruptedException {
+        organization.click();
+        Waits.shortPause();
+        locations.click();
+        addlocation.click();
+        Waits.shortPause();
+        locationname.sendKeys(Name);
+        Waits.shortPause();
+        locationcity.sendKeys(City);
+        Waits.shortPause();
+        locationprovince.sendKeys(Province);
+        Waits.shortPause();
+        selectCountry(SelectedCountry);
+        Waits.shortPause();
+        locationphone.sendKeys(Phone);
+        Waits.shortPause();
+        locationfax.sendKeys(Fax);
+        Waits.shortPause();
+        locationaddress.sendKeys(sendAddress);
+        Waits.shortPause();
+        locationnotes.sendKeys(LocationNotes);
+        Waits.shortPause();
+        save.click();
+    }
+
+    public WebElement searchORGStructure(String DeptName) {
+        WebElement OrgStructureResults = structureElements.stream().filter(orgresult -> orgresult.getText().contains(DeptName)).findFirst().orElse(null);
+        return OrgStructureResults;
+    }
+
+    public void changeOrganizationStructure(String DeptName, String UnitOperation) throws InterruptedException {
+        organization.click();
+        Waits.shortPause();
+        structure.click();
+        Waits.shortPause();
+        generaleditcheckbox.click();
+        Waits.shortPause();
+        OrgResults = searchORGStructure(DeptName);
+        Waits.shortPause();
+        if (UnitOperation.contains("Delete")) {
+            OrgResults.findElement(strDelete);
+        } else if (UnitOperation.contains("Edit")) {
+            OrgResults.findElement(strEdit);
+        } else if (UnitOperation.contains("AddUnit")) {
+            OrgResults.findElement(strAddUnit);
+        } else {
+            System.out.println("Entered Value is not match");
+            Assert.fail("Entered Value is not match");
+        }
+    }
+
+    public void addOrganizationUnit(String UnitID, String UnitName, String UnitDesc) throws InterruptedException {
+        organization.click();
+        Waits.shortPause();
+        structure.click();
+        Waits.shortPause();
+        generaleditcheckbox.click();
+        Waits.shortPause();
+        addorganization.click();
+        Waits.shortPause();
+        addunitid.sendKeys(UnitID);
+        Waits.shortPause();
+        addunitname.sendKeys(UnitName);
+        Waits.shortPause();
+        addunitdesc.sendKeys(UnitDesc);
+        Waits.shortPause();
+        unitsave.click();
+    }
 }
-
-
